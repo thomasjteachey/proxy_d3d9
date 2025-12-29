ee0da5c7e1172e8f12543541beffc8a6700c3141
thomasjteachey <81936627+thomasjteachey@users.noreply.github.com>
2025-12-29 14:28:19 -0500

    Fix FrameFence render thread helpers include

--- src/hitgate.h
+++ src/hitgate.h
@@ -9,3 +9,7 @@ void HitGate_ArmOneFrame();
 bool HitGate_TryDeferSVK(void* self, int a1, int a2, SVKStarter_t orig);
 bool HitGate_IsEnabled();
 void HitGate_SetRenderThreadId(uint32_t tid);
+uint32_t HitGate_GetDispatchHits();
+uint32_t HitGate_GetSaw14A();
+uint32_t HitGate_GetLastOpcode();
+uint32_t HitGate_GetLastDispatchTid();
